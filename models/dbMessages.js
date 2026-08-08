97001d1c79bf62eb1a9a83f9247e19620316d3ff
olopok <pabisa.pb@gmail.com>
2026-08-08 20:21:40 +0100

    last commit

--- models/dbMessages.js
+++ models/dbMessages.js
@@ -2,7 +2,7 @@ const messages = [
   {
     text: "Hi there!",
     user: "Armando",
-    added: new Date().toLocaleDateString(),
+    added: new Date().toLocaleString(),
     id: crypto.randomUUID(),
   },
   {
