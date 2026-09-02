da7fed4f24ac692ed61fb4e20dc80d4ed7e0384a
olopok <pabisa.pb@gmail.com>
2026-09-02 14:46:37 +0100

    test3

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ const dotenv = require("dotenv");
 dotenv.config();
 const pool = require("./db/pool");
 
-// const boardMessages = require("./routes/board");
+const boardMessages = require("./routes/board");
 // const newMessage = require("./routes/new");
 // const detailsMsg = require("./routes/details");
 const path = require("node:path");
 const assetsPath = path.join(__dirname, "public");
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
@@ -17,18 +17,9 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(assetsPath));
 
-// app.use("/", boardMessages);
+app.use("/", boardMessages);
 // app.use("/new", newMessage);
 // app.use("/:messageId", detailsMsg);
-app.get("/", async (req, res) => {
-  try {
-    const result = await pool.query("SELECT NOW()");
-    res.render("index", { time: result.rows[0].now });
-  } catch (err) {
-    console.error(err);
-    res.status(500).send("Errore database");
-  }
-});
 
 app.listen(PORT, (error) => {
   if (error) {
--- routes/board.js
+++ routes/board.js
@@ -1,16 +1,25 @@
 const { Router } = require("express");
-const messages = require("../models/dbMessages");
-const links = require("../models/navlinks");
+// const messages = require("../models/dbMessages");
+// const links = require("../models/navlinks");
 
 const messagesRouter = Router();
 
-messagesRouter.get("/", (req, res) =>
-  res.render("index", {
-    title: "Mini Message Board",
-    messages: messages,
-    links: links,
-  }),
-);
+// messagesRouter.get("/", (req, res) =>
+//   res.render("index", {
+//     title: "Mini Message Board",
+//     messages: messages,
+//     links: links,
+//   }),
+// );
+messagesRouter.get("/", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT NOW()");
+    res.render("index", { time: result.rows[0].now });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Errore database");
+  }
+});
 
 
 
